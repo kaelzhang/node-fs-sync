8b1c67a697661fd274c681f0b5756490aba5610a
Kael <i@kael.me>
2013-05-31 20:10:52 +0800

    add `fs.doesPathContain` method; update package.json

--- dist/index.js
+++ dist/index.js
@@ -44,6 +44,24 @@ function processPatterns(patterns, fn) {
 };
 
 
+// Are descendant path(s) contained within ancestor path? Note: does not test
+// if paths actually exist.
+sync.doesPathContain = function(ancestor) {
+	ancestor = node_path.resolve(ancestor);
+	
+	var relative;
+	var i = 1;
+
+	for (; i < arguments.length; i ++) {
+		relative = node_path.relative(node_path.resolve(arguments[i]), ancestor);
+		if (relative === '' || /\w+/.test(relative)) {
+			return false;
+		}
+	}
+
+	return true;
+};
+
 
 
 // @param {Object} options {
